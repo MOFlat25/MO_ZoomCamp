25c95bbdebe8a85b2d3b7afc9dd2b4af3ec79a96
MOFlat25 <Marisa.OFlaherty@gmail.com>
2024-12-23 18:46:50 +0000

    committed on 23 Dec

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -1,11 +1,11 @@
+import pandas as pandas
 import sys
 
-import pandas as pd
-
+# sys.argv allows to pass arguments to the script from the commandline
 print(sys.argv)
 
+# sys.argv[0] > name of the file
+# sys.argv[1] > first argument passed
 day = sys.argv[1]
 
-# some fancy stuff with pandas
-
-print(f'job finished successfully for day = {day}')
+print(f"Job finished successfully for day = {day}")
